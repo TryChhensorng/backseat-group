9148d9651e2a1333ce1be7c0e29b96ca281852e0
Pochhay <141006884+Pochhay@users.noreply.github.com>
2024-04-05 15:11:09 +0700

    1,2,3

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,10 @@
 def replace_last(numbers):
+    temp = numbers[:]
+    last = len(numbers)-1
+    temp[0]=numbers[last]
+    for i in range(len(numbers)-1):
+        temp[i+1] = numbers[i]
+    return temp
     ...
+
+print(replace_last([1,2,3,4,5,6]))
