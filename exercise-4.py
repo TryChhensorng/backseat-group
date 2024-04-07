072d8320ad67435fd76b5984c9dc1ca740e5ff26
ChhensorngTry <2024077try@aupp.edu.kh>
2024-04-07 23:26:18 +0700

    submit

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,12 @@
+from math import ceil
 def chunking_by(numbers, chunck):
-    ...
+    temp = []
+    for i in range(ceil(len(numbers)/chunck)):
+        x = i*chunck
+        if len(numbers) < x + chunck:
+            a = numbers[x:len(numbers)]
+        else:
+            a = numbers[x:x+chunck]
+        temp.append(a)
+    return temp
+    ... 
